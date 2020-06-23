0128050a1d9518eb2199417cc09fbeaa2ead4148
Andrew Zhang <zandy99@gmail.com>
2020-06-22 23:21:40 -0400

    the buttons work, there's nothing on the pages yet

--- src/main.py
+++ src/main.py
@@ -5,7 +5,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/create")
+def create_account():
+    return render_template("create_account.html")
 
 
 if __name__ == "__main__":
